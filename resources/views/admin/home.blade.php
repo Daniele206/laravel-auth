94c1c618c2bdaf98b4b2d0a67a4564b3cbab5b1f
Daniele206 <danieledeieso2004@gmail.com>
2024-05-22 15:48:39 +0200

    [add] model, migration "Project"

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,5 +1,12 @@
 @extends('layouts.admin')
 
 @section('content')
-<h1>Home-admin</h1>
+<div class="container-fluid cont_h">
+    <div class="row h-100">
+        <div class="col col-2 bg-danger h-100"></div>
+        <div class="col bg-success h-100 overflow-auto">
+
+        </div>
+    </div>
+</div>
 @endsection
